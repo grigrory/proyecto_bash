d6cb9cae3e07a1049a3b1a6d80e51c4cc4c498d5
angels <qura2910@gmail.com>
2025-04-09 17:36:22 -0600

    primera versión poblacion-clima

---
+++ pregunton.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+echo "hola, pregunto cosas"
+echo "población de Mexico?"
+grep "Mexico" paises.txt | -d"," -f2
+grep "Mexico" paises.txt | -d"," -f2
+grep "Mexico" paises.txt | cut -d"," -f2
+echo "Cual es el clima de Mexico?"
+grep "Mexico" paises.txt | cut -d"," -f3
